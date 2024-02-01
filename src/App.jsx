ae3a724f38f77f712e908711ccfc8c303ffe358b
Christopher Cali <chris@consensusnetworks.com>
2024-02-01 17:17:35 -0500

    Add some more scaffold + Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from './pages/landing/Landing';
 
 function App() {
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
