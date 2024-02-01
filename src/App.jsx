10363cc0f0c037c4b831f51675b8e3edb597eeb6
Christopher Cali <chris@consensusnetworks.com>
2024-02-01 18:02:26 -0500

    Format Landing and do Typescale

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
+import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing } from './pages/landing/Landing';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app_container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
